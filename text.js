ca9bfafd4c714dda8e8426f8e0b5f39803b09a37
sk-0816 <kouhei17n1100371@nnn.ed.js>
2017-07-03 15:49:15 +0900

    リスト 6-17を反映

--- text.js
+++ text.js
@@ -1,6 +1,14 @@
-document.addEventListener('DOMContentLoaded',function(){
+/*document.addEventListener('DOMContentLoaded',function(){
     document.getElementById('result_text').textContent =
         '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
         document.getElementById('result_html').innerHTML =
         '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+},false);*/
+document.addEventListener('DOMContentLoaded',function(){
+    document.getElementById('btn').addEventListener('click',function(){
+        let name = document.getElementById('name');
+        let result = document.getElementById('result');
+//      result.innerHTML = 'こんにちは、'+ name.value + 'さん!'; ←プログラムを打ち込まれるとそれを実装してしまう。
+        result.textContent = 'こんにちは、'+ name.value + 'さん!'; 
+    },false);
 },false);

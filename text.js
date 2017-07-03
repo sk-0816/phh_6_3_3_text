1833baf6f80c652d27a692d36e0334d022246a87
sk-0816 <kouhei17n1100371@nnn.ed.js>
2017-07-03 15:01:24 +0900

    リスト6-16

--- text.js
+++ text.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
-    document.getElementById('result_text').textContent =
-        '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
-        document.getElementById('result_html').innerHTML =
-        '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+    let list = document.getElementById('list');
+    console.log(list.innerHTML);
+    console.log(list.textContent);
 },false);
